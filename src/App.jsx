3ce1ab7a112321e2eba93eeeb31972cafc00ebd5
rblanc6 <rab.nola@gmail.com>
2024-11-21 20:33:11 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
